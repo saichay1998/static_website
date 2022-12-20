62f9301445d3b30052cbc646821c590a44fb7aac
sreekanth1096 <30802769+sreekanth1096@users.noreply.github.com>
2022-12-20 12:26:08 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -17,9 +17,9 @@ terraform {
 
 provider "aws" {
   assume_role {
-    role_arn     = "arn:aws:iam::534083224817:role/aws-service-role/support.amazonaws.com/AWSServiceRoleForSupport"
-#     session_name = "SESSION_NAME"
-#     external_id  = "EXTERNAL_ID"
+#     role_arn     = "arn:aws:iam::534083224817:role/aws-service-role/support.amazonaws.com/AWSServiceRoleForSupport"
+# #     session_name = "SESSION_NAME"
+# #     external_id  = "EXTERNAL_ID"
   }
   region = "ap-northeast-1"
 }
