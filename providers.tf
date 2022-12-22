7f954010803fd88ff000322b1ce22c305c6f7401
saichay1998 <88181990+saichay1998@users.noreply.github.com>
2022-12-22 11:40:57 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "mystatestoringbucket"
+    bucket = "bucketterraformstatic"
     key    = "state/terraform.tfstate"
     region = "ap-northeast-1"
   }
